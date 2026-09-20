5b07ebd2e3e0c87a10aaf1f1bdaee5ab60e32dda
breno-hof <brenohenrique.oliveiraferreira@gmail.com>
2026-09-20 09:08:48 +0000

    fix: babele register

--- scripts/babele.js
+++ scripts/babele.js
@@ -1,14 +1,18 @@
 Hooks.once("init", () => {
   if (typeof Babele === "undefined") {
-    console.warn(
+    console.error(
       "D&D - Heroes of the Borderlands PT-BR | Babele não está instalado ou ativo."
     );
     return;
   }
 
   Babele.get().register({
-    module: "dnd-heroes-borderlands-pt-br",
+    module: "dnd-heroes-borderlands-translation-pt-br",
     lang: "pt-BR",
     dir: "compendium"
   });
+
+  console.log(
+    "D&D - Heroes of the Borderlands PT-BR | Traduções Babele registradas."
+  );
 });
